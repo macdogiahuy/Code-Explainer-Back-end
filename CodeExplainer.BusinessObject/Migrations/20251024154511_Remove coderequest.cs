5671de338c14df34a441aff804728560cc31b39b
kleqing <nhanpk28122004@gmail.com>
2025-10-24 23:51:53 +0700

    [Update] Update framework to 9.0

---
+++ CodeExplainer.BusinessObject/Migrations/20251024154511_Remove coderequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeExplainer.BusinessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removecoderequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodeRequests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodeRequests",
+                columns: table => new
+                {
+                    CodeRequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AIResponse = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Language = table.Column<string>(type: "text", nullable: false),
+                    PromptType = table.Column<string>(type: "text", nullable: false),
+                    SourceCode = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeRequests", x => x.CodeRequestId);
+                    table.ForeignKey(
+                        name: "FK_CodeRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeRequests_UserId",
+                table: "CodeRequests",
+                column: "UserId");
+        }
+    }
+}
